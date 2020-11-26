86569a20489b1b936c6be8c92410da7d4a19853a
tnrat-ra <75009328+tnrat-ra@users.noreply.github.com>
2020-11-26 16:59:28 +0530

    Create bash metasploit.sh

---
+++ bash metasploit.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+pkg inatall ruby
+pkg install php
+pkg install figlet
+cd
+clear
+gem install lolcat
+figlet Metasploit |lolcat|lolcat
+pkg install unstable-repo
+pkg install x11-repo
+clear
+figlet Prince Yasin |lolcat|lolcat
+pkg install metasploit
